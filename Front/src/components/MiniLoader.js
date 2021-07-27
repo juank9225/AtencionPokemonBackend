03143099329e10e66803a448a2b81a6e27df2599
Santiago Mejia <santiagomejiad19@hotmail.com>
2021-07-27 02:37:10 -0500

    algunos cambios

--- Front/src/components/MiniLoader.js
+++ Front/src/components/MiniLoader.js
@@ -1,14 +1,32 @@
 import React, { Component } from 'react';
 
 import './styles/Loader.css';
+import uno from '../images/uno.svg';
+import dos from '../images/dos.svg';
+import tres from '../images/tres.svg';
 
 export default class MiniLoader extends Component {
   render() {
     return (
       <div className="lds-grid">
-        <div />
-        <div />
-        <div />
+        <div> 
+          <img
+            src={uno}
+            alt="circulo"
+            className="circulo"/>
+        </div>
+        <div> 
+          <img
+            src={dos}
+            alt="circulo"
+            className="circulo"/>
+        </div>
+        <div> 
+          <img
+            src={tres}
+            alt="circulo"
+            className="circulo"/>
+        </div>
       </div>
     );
   }
