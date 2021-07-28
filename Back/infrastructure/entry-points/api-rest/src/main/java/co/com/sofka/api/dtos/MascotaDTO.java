4c75b85dfc67c4639dd07a3e359531936c59f2e4
juan carlos salcedo <juank9225@gmail.com>
2021-07-28 15:29:00 -0500

    Se agrega el dto y el mapper de la mascota

--- Back/infrastructure/entry-points/api-rest/src/main/java/co/com/sofka/api/dtos/MascotaDTO.java
+++ Back/infrastructure/entry-points/api-rest/src/main/java/co/com/sofka/api/dtos/MascotaDTO.java
@@ -1,4 +1,82 @@
 package co.com.sofka.api.dtos;
 
+import co.com.sofka.model.consulta.values.valueobjectmascota.*;
+
+import java.util.List;
+
 public class MascotaDTO {
+    private String id;
+    private Identificacion identificacion;
+    private Nombre nombre;
+    private Raza raza;
+    private List<Tipo> tipo;
+    private Habilidad habilidad;
+
+    public MascotaDTO() {
+    }
+
+    public MascotaDTO(String id, Identificacion identificacion, Nombre nombre, Raza raza, List<Tipo> tipo, Habilidad habilidad) {
+        this.id = id;
+        this.identificacion = identificacion;
+        this.nombre = nombre;
+        this.raza = raza;
+        this.tipo = tipo;
+        this.habilidad = habilidad;
+    }
+
+    public MascotaDTO(Identificacion identificacion, Nombre nombre, Raza raza, List<Tipo> tipo, Habilidad habilidad) {
+        this.identificacion = identificacion;
+        this.nombre = nombre;
+        this.raza = raza;
+        this.tipo = tipo;
+        this.habilidad = habilidad;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public Identificacion getIdentificacion() {
+        return identificacion;
+    }
+
+    public void setIdentificacion(Identificacion identificacion) {
+        this.identificacion = identificacion;
+    }
+
+    public Nombre getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(Nombre nombre) {
+        this.nombre = nombre;
+    }
+
+    public Raza getRaza() {
+        return raza;
+    }
+
+    public void setRaza(Raza raza) {
+        this.raza = raza;
+    }
+
+    public List<Tipo> getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(List<Tipo> tipo) {
+        this.tipo = tipo;
+    }
+
+    public Habilidad getHabilidad() {
+        return habilidad;
+    }
+
+    public void setHabilidad(Habilidad habilidad) {
+        this.habilidad = habilidad;
+    }
 }
--- Back/infrastructure/entry-points/api-rest/src/main/java/co/com/sofka/api/mapper/MascotaMapper.java
+++ Back/infrastructure/entry-points/api-rest/src/main/java/co/com/sofka/api/mapper/MascotaMapper.java
@@ -1,4 +1,52 @@
 package co.com.sofka.api.mapper;
 
+import co.com.sofka.api.dtos.MascotaDTO;
+import co.com.sofka.model.consulta.entity.mascotapokemon.MascotaPokemon;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
 public class MascotaMapper {
+
+    public MascotaPokemon fromDTO(MascotaDTO mascotaDTO){
+        MascotaPokemon mascotaPokemon = new MascotaPokemon();
+        mascotaPokemon.setId(mascotaDTO.getId());
+        mascotaPokemon.setIdentificacion(mascotaDTO.getIdentificacion());
+        mascotaPokemon.setNombre(mascotaDTO.getNombre());
+        mascotaPokemon.setRaza(mascotaDTO.getRaza());
+        mascotaPokemon.setTipo(mascotaDTO.getTipo());
+        mascotaPokemon.setHabilidad(mascotaDTO.getHabilidad());
+
+        return mascotaPokemon;
+    }
+
+    public MascotaDTO fromModel(MascotaPokemon mascotaPokemon){
+        MascotaDTO mascotaDTO = new MascotaDTO();
+        mascotaDTO.setId(mascotaPokemon.getId());
+        mascotaDTO.setIdentificacion(mascotaPokemon.getIdentificacion());
+        mascotaDTO.setNombre(mascotaPokemon.getNombre());
+        mascotaDTO.setRaza(mascotaPokemon.getRaza());
+        mascotaDTO.setTipo(mascotaPokemon.getTipo());
+        mascotaDTO.setHabilidad(mascotaPokemon.getHabilidad());
+
+        return mascotaDTO;
+    }
+
+    public List<MascotaDTO> fromCollectionList(List<MascotaPokemon> collection) {
+        if (collection == null) {
+            return null;
+
+        }
+        List<MascotaDTO> list = new ArrayList(collection.size());
+        Iterator listTracks = collection.iterator();
+
+        while(listTracks.hasNext()) {
+            MascotaPokemon mascotaPokemon = (MascotaPokemon) listTracks.next();
+            list.add(fromModel(mascotaPokemon));
+        }
+
+        return list;
+    }
+
 }
