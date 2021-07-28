8edaa89d9268fca3500fe2f66ce61846ee3766f4
Santiago Mejia <santiagomejiad19@hotmail.com>
2021-07-28 01:00:30 -0500

    Se Agrega servicio Post para el agregado Atencion

--- Back/domain/model/src/main/java/co/com/sofka/model/atencion/gateways/AtencionRepository.java
+++ Back/domain/model/src/main/java/co/com/sofka/model/atencion/gateways/AtencionRepository.java
@@ -1,4 +1,7 @@
 package co.com.sofka.model.atencion.gateways;
 
+import co.com.sofka.model.atencion.Atencion;
+
 public interface AtencionRepository {
+    Atencion crearAtencion(Atencion atencion);
 }
