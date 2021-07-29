bba34252c7cbfc32babd1f3ae0c5d3f177b5f6ee
juan carlos salcedo <juank9225@gmail.com>
2021-07-28 20:53:43 -0500

    Se agrega la funcionabilidad de obtener por id del usuario

---
+++ Back/infrastructure/entry-points/api-rest/src/main/java/co/com/sofka/api/apirest/consultaapirest/usuarioapirest/ListarUsuarioIdApiRest.java
@@ -0,0 +1,27 @@
+package co.com.sofka.api.apirest.consultaapirest.usuarioapirest;
+
+import co.com.sofka.api.dtos.UsuarioDTO;
+import co.com.sofka.api.mapper.UsuarioMapper;
+import co.com.sofka.usecase.consulta.usuario.ListarUsuarioIdUseCase;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/api", produces = MediaType.APPLICATION_JSON_VALUE)
+@AllArgsConstructor
+public class ListarUsuarioIdApiRest {
+    private final ListarUsuarioIdUseCase listarUsuarioIdUseCase;
+    private UsuarioMapper usuarioMapper;
+
+    @GetMapping(path = "/listar/usuario/{id}")
+    public ResponseEntity<UsuarioDTO> listarUsuarioId(@PathVariable("id") String id){
+        return new ResponseEntity(usuarioMapper.fromModel(listarUsuarioIdUseCase.obtenerUsuario(id)), HttpStatus.OK);
+    }
+
+}
