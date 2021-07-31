4921ab27e8e320a11628c3660fb38ca0a3499a49
Ana María <anahernandez814@gmail.com>
2021-07-31 18:42:25 -0500

    Se crea test para crear mascota pokemon

---
+++ Back/domain/usecase/src/test/java/co/com/sofka/usecase/consulta/mascota/CreateMascotaUseCaseTest.java
@@ -0,0 +1,40 @@
+package co.com.sofka.usecase.consulta.mascota;
+
+import co.com.sofka.model.consulta.entity.mascotapokemon.MascotaPokemon;
+import co.com.sofka.model.consulta.entity.mascotapokemon.gateways.MascotaPokemonRepository;
+import co.com.sofka.model.consulta.values.valueobjectmascota.*;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.boot.test.mock.mockito.SpyBean;
+
+@SpringBootTest(classes = CreateMascotaUseCase.class)
+class CreateMascotaUseCaseTest {
+    @MockBean
+    private MascotaPokemonRepository mascotaPokemonRepository;
+
+    @SpyBean
+    private CreateMascotaUseCase createMascotaUseCase;
+
+    @Test
+    public void testsCrearMascota() {
+        MascotaPokemon mascotaPokemon = new MascotaPokemon("xxxx",
+                new Identificacion(1786411L),
+                new Nombre("Kali"),
+                new Raza("Sandshrew"),
+                new Tipo("Tierra"),
+                new Habilidad("Velo Arena")
+        );
+        Mockito.when(mascotaPokemonRepository.crearMascota(mascotaPokemon)).thenReturn(mascotaPokemon);
+        MascotaPokemon resp = createMascotaUseCase.crearMascota(mascotaPokemon);
+
+        Assertions.assertEquals(resp.getId(), "xxxx");
+        Assertions.assertEquals(resp.getIdentificacion().getValor(),(Long) 1786411L);
+        Assertions.assertEquals(resp.getNombre().getValor(), "Kali");
+        Assertions.assertEquals(resp.getRaza().getValor(), "Sandshrew");
+        Assertions.assertEquals(resp.getTipo().getValor(), "Tierra");
+        Assertions.assertEquals(resp.getHabilidad().getValor(), "Velo Arena");
+    }
+}
